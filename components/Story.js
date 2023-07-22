a3b695858b31f488df5fa48ee475afd0a360fb35
CodeLuca <lucamarchal@gmail.com>
2023-07-22 23:28:08 +0200

    fix

--- components/Story.js
+++ components/Story.js
@@ -22,16 +22,16 @@ export default function Story({ story }) {
   return (
     <div className="w-full flex story-container md:flex-row flex-col">
       <div className="md:w-1/3 flex justify-center grayscale hover:grayscale-0">
-      <a href={story.link} target="_blank" rel="noopener noreferrer">
-  <img src={srcImg} className="md:pr-9" alt="Story Image" />
-</a>
+        <a href={story.link} target="_blank" rel="noopener noreferrer">
+          <img src={srcImg} className="md:pr-9" alt="Story Image" />
+        </a>
       </div>
       <div className="md:w-2/3 flex flex-col justify-center h-100 pt-4 md:pt-0">
         <div>{story.creator} - {formattedDate}</div>
         <div className="display-font text-4xl">{story.title}</div>
         <div className="pb-4 pt-2">{story.contentSnippet}</div>
         <div className="flex">
-          {story.tags.map((tag, index) => (
+          {story.tags?.map((tag, index) => (
             <div key={index} className={`${index % 2 === 0 ? "bg-blue-300" : "bg-green-300"} px-2 mr-5`}>{tag}</div>
           ))}
         </div>
