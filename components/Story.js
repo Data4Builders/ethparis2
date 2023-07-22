f5f32cfee53b6410f939f99cf8df77be2ca322ca
JordyDutch <jordydutch1@gmail.com>
2023-07-22 13:07:21 +0200

    Add article details and analytics

--- components/Story.js
+++ components/Story.js
@@ -1,6 +1,7 @@
+import React from 'react';
+import moment from 'moment';
 
 export default function Story({ story }) {
-
 
   function extractSrcFromHTML(htmlString) {
     const regex = /<img[^>]+src="([^">]+)"/;
@@ -15,13 +16,16 @@ export default function Story({ story }) {
 
   const srcImg = extractSrcFromHTML(story.content);
 
+  // Use moment.js to format the dates
+  const formattedDate = moment(story.pubDate).fromNow();
+
   return (
     <div className="w-full flex story-container md:flex-row flex-col">
       <div className="md:w-1/3 flex justify-center grayscale hover:grayscale-0">
         <img src={srcImg} className="md:pr-9" alt="Story Image" />
       </div>
       <div className="md:w-2/3 flex flex-col justify-center h-100 pt-4 md:pt-0">
-        <div>{story.creator}</div>
+        <div>{story.creator} - {formattedDate}</div>
         <div className="display-font text-4xl">{story.title}</div>
         <div className="pb-4 pt-2">{story.contentSnippet}</div>
         <div className="flex">
